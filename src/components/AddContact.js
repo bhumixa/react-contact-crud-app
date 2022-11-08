254e45cd5c105ec106593ea70c44c6cbf1ab3428
bhumixa <bhumi.patel2512@gmail.com>
2022-11-08 15:18:00 +0530

    navigate with class component and appjs changes

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -4,106 +4,32 @@ import { v4 as uuidv4 } from 'uuid';
 import ReactDOM from 'react-dom';
 import { useLocation, Navigate, useNavigate } from 'react-router-dom'
 
-const AddContact = (props) => {
-    const myuuid = uuidv4();
-    const [contact, setContact] = useState({
-        id: myuuid,
-        name: "",
-        email: ""
-    })
-    const navigate = useNavigate()
-
-
-    const addNewContact = async () => {
-        
-        console.log(contact)
-        await props.newContactHandler(contact)
-        navigate('/')
-        
-        // setTimeout(async()=>{           
-        //     console.log(contact)
-           
-        //     navigate('/')
-        // }, 1000)
-        
-       
-        // await this.setState({ id: myuuid })
-        // console.log(this.state)
-        // this.props.newContactHandler(this.state)
-
-        // useEffect(() => {
-        //     const navigate = useNavigate();
-        //     navigate('/')
-        // }, []);
-
-    }
-    return (
-        <>
-            <div className="my-3 container">
-                <div className="card col-md-12 mx-auto" style={{ width: '18rem' }}>
-                    <h3 className='mx-3 my-3'> Add New Contact</h3>
-
-                    <div className="row justify-content-md-center">
-                        <form className="row g-3 container " >
-                            <div className="row  my-2">
-                                <div className="col">
-                                    <label htmlFor="staticEmail2" >Name</label>
-                                    <input type="text" className="form-control" id="name" onChange={async (e) => await setContact({ ...contact, name: e.target.value })} />
-                                </div>
-                            </div>
-                            <div className="row  my-2">
-                                <div className="col">
-                                    <label htmlFor="inputPassword2" >Email</label>
-                                    <input type="text" className="form-control" id="email" placeholder="Email" onChange={async (e) => await setContact({ ...contact, email: e.target.value })} />
-                                </div>
-                            </div>
-                            <div className="row  my-2">
-                                <div className="col">
-                                    <button type="button" className="btn btn-primary mb-3" onClick={() => addNewContact()}>Save</button>
-                                </div>
-                            </div>
-
-                        </form>
-                    </div>
-                </div>
-
-            </div>
-        </>
-    )
-
-}
-
-// export class AddContact extends Component {
-
-//     static propTypes = {
-//         id: "",
+// const AddContact = (props) => {
+//     const myuuid = uuidv4();
+//     const [contact, setContact] = useState({
+//         id: myuuid,
 //         name: "",
 //         email: ""
-//     }
-
-//     constructor(props) {
-//         super(props)
-//         this.state = {
-//             id: '',
-//             name: '',
-//             email: ''
-//         }
-//     }
-//     componentDidMount = () => {
-//         useEffect(() => {
-//             const navigate = useNavigate();
-//             navigate('/')
-//         }, []);
-//     }
+//     })
+//     const navigate = useNavigate()
 
 
-//     addContact = async () => {
-
-//         let myuuid = await uuidv4();
-//         console.log(myuuid)
-//         await this.setState({ id: myuuid })
-//         console.log(this.state)
-//         this.props.newContactHandler(this.state)
+//     const addNewContact = async () => {
+        
+//         console.log(contact)
+//         await props.newContactHandler(contact)
+//         navigate('/')
+        
+//         // setTimeout(async()=>{           
+//         //     console.log(contact)
+           
+//         //     navigate('/')
+//         // }, 1000)
+        
+       
+//         // await this.setState({ id: myuuid })
+//         // console.log(this.state)
+//         // this.props.newContactHandler(this.state)
 
 //         // useEffect(() => {
 //         //     const navigate = useNavigate();
@@ -111,42 +37,103 @@ const AddContact = (props) => {
 //         // }, []);
 
 //     }
+//     return (
+//         <>
+//             <div className="my-3 container">
+//                 <div className="card col-md-12 mx-auto" style={{ width: '18rem' }}>
+//                     <h3 className='mx-3 my-3'> Add New Contact</h3>
 
-//     render() {
-//         return (
-//             <>
-//                 <div className="my-3 container">
-//                     <div className="card col-md-12 mx-auto" style={{ width: '18rem' }}>
-//                         <h3 className='mx-3 my-3'> Add New Contact</h3>
+//                     <div className="row justify-content-md-center">
+//                         <form className="row g-3 container " >
+//                             <div className="row  my-2">
+//                                 <div className="col">
+//                                     <label htmlFor="staticEmail2" >Name</label>
+//                                     <input type="text" className="form-control" id="name" onChange={async (e) => await setContact({ ...contact, name: e.target.value })} />
+//                                 </div>
+//                             </div>
+//                             <div className="row  my-2">
+//                                 <div className="col">
+//                                     <label htmlFor="inputPassword2" >Email</label>
+//                                     <input type="text" className="form-control" id="email" placeholder="Email" onChange={async (e) => await setContact({ ...contact, email: e.target.value })} />
+//                                 </div>
+//                             </div>
+//                             <div className="row  my-2">
+//                                 <div className="col">
+//                                     <button type="button" className="btn btn-primary mb-3" onClick={() => addNewContact()}>Save</button>
+//                                 </div>
+//                             </div>
 
-//                         <div className="row justify-content-md-center">
-//                             <form className="row g-3 container " >
-//                                 <div className="row  my-2">
-//                                     <div className="col">
-//                                         <label htmlFor="staticEmail2" >Name</label>
-//                                         <input type="text" className="form-control" id="name" onChange={(e) => this.state.name = e.target.value} />
-//                                     </div>
-//                                 </div>
-//                                 <div className="row  my-2">
-//                                     <div className="col">
-//                                         <label htmlFor="inputPassword2" >Email</label>
-//                                         <input type="text" className="form-control" id="email" placeholder="Email" onChange={(e) => this.state.email = e.target.value} />
-//                                     </div>
-//                                 </div>
-//                                 <div className="row  my-2">
-//                                     <div className="col">
-//                                         <button type="button" className="btn btn-primary mb-3" onClick={() => this.addContact()}>Save</button>
-//                                     </div>
-//                                 </div>
-
-//                             </form>
-//                         </div>
+//                         </form>
 //                     </div>
-
 //                 </div>
-//             </>
-//         )
-//     }
+
+//             </div>
+//         </>
+//     )
+
 // }
+
+export class AddContact extends Component {
+
+    static propTypes = {
+        id: "",
+        name: "",
+        email: ""
+    }
+
+    constructor(props) {
+        let myuuid = uuidv4();
+        
+        super(props)
+        this.state = {
+            id: myuuid,
+            name: '',
+            email: ''
+        }
+        console.log(this.props)
+    }
+
+    addContact = async () => {
+        console.log(this.state)
+        this.props.newContactHandler(this.state)
+        this.props.navigate('/')
+    }
+
+    render() {
+        return (
+            <>
+                <div className="my-3 container">
+                    <div className="card col-md-12 mx-auto" style={{ width: '18rem' }}>
+                        <h3 className='mx-3 my-3'> Add New Contact</h3>
+
+                        <div className="row justify-content-md-center">
+                            <form className="row g-3 container " >
+                                <div className="row  my-2">
+                                    <div className="col">
+                                        <label htmlFor="staticEmail2" >Name</label>
+                                        <input type="text" className="form-control" id="name" onChange={(e) => this.state.name = e.target.value} />
+                                    </div>
+                                </div>
+                                <div className="row  my-2">
+                                    <div className="col">
+                                        <label htmlFor="inputPassword2" >Email</label>
+                                        <input type="text" className="form-control" id="email" placeholder="Email" onChange={(e) => this.state.email = e.target.value} />
+                                    </div>
+                                </div>
+                                <div className="row  my-2">
+                                    <div className="col">
+                                        <button type="button" className="btn btn-primary mb-3" onClick={() => this.addContact()}>Save</button>
+                                    </div>
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+            </>
+        )
+    }
+}
 
 export default AddContact
--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,58 @@ import { useState } from 'react';
 import ContactDetails from './ContactDetails';
 import { useLocation, Navigate, useNavigate } from 'react-router-dom'
 
+// function App() {
+//   const navigate = useNavigate()
+//   //contactList = [];
+//   const [contacts, setContact] = useState([]);
+
+//   const newContactAdded = async (data) => {
+//     console.log('---add', data)
+//     let newContact = [...contacts]
+
+//     console.log(newContact)
+//     await newContact.push(data)
+//     await setContact(newContact)
+
+
+//     console.log(contacts)
+//   }
+
+//   const contactListUpdated = async (contacts) => {
+//     console.log('contacts')
+//     console.log(contacts)
+//     await setContact(contacts)
+//   }
+
+//   return (
+
+//     <>
+//       <div className="container">
+//         <BrowserRouter>
+//           <Routes>
+//             <Route exact path="/" element={<Contacts contactList={contacts} contactListUpdatedHandler={contactListUpdated} />}></Route>
+//             <Route exact path="add" element={<AddContact newContactHandler={newContactAdded} />} > </Route>
+//             <Route exact path="contact/:id" element={<ContactDetails />} > </Route>
+//           </Routes>
+
+
+//         </BrowserRouter >
+//       </div>
+//     </>
+
+//   );
+// }
 function App() {
-  //const navigate = useNavigate()
-  //contactList = [];
+  return (
+    <BrowserRouter>
+      <RouterComponent />
+    </BrowserRouter>
+  )
+}
+
+function RouterComponent() {
   const [contacts, setContact] = useState([]);
+  const navigate = useNavigate();
 
   const newContactAdded = async (data) => {
     console.log('---add', data)
@@ -25,28 +73,20 @@ function App() {
   }
 
   const contactListUpdated = async (contacts) => {
-    console.log('contacts')
     console.log(contacts)
     await setContact(contacts)
   }
 
   return (
-    
-    <>
-      <div className="container">
-        <BrowserRouter>
-          <Routes>
-            <Route exact path="/" element={<Contacts contactList={contacts} contactListUpdatedHandler={contactListUpdated} />}></Route>
-            <Route exact path="add" element={<AddContact newContactHandler={newContactAdded} />} > </Route>
-            <Route exact path="contact/:id" element={<ContactDetails />} > </Route>
-          </Routes>
-
-
-        </BrowserRouter >
-      </div>
-    </>
-
-  );
+    <div className="App">
+      <>
+        <Routes>
+          <Route exact path="/" element={<Contacts contactList={contacts} contactListUpdatedHandler={contactListUpdated} />}></Route>
+          <Route exact path="add" element={<AddContact newContactHandler={newContactAdded} navigate={navigate} />} > </Route>
+          <Route exact path="contact/:id" element={<ContactDetails />} > </Route>
+        </Routes>
+      </>
+    </div>
+  )
 }
-
 export default App;
